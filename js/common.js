0ff4425a8099dfb9fbb1802fe87c129df5b61c6d
Sanwer <sanwer@qq.com>
2022-05-11 18:43:39 +0800

    更新百度统计代码

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8bf601f5bf00946916771069c85798be";
+  hm.src = "https://hm.baidu.com/hm.js?ad1d0e117c36acdab4e62ee19ebca48c";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
